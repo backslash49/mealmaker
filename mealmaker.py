c9ef4b7582edcc4c7754b60c7f5edbfb7c6663da
Michael <michaelmccormack49@gmail.com>
2016-01-24 18:59:36 -0500

    Mealmaker working better, added function to import csv

--- mealmaker.py
+++ mealmaker.py
@@ -1,26 +1,37 @@
 import re
 import sys
+import openascsv
+
 
 
 recipes = {}
-
 recipes['soup'] = ('salt', 'water', 'stock')
+recipes['grilled cheese'] = ('bread', 'butter', 'cheese')
+recipes['pb&j'] = ('peanut-butter', 'jelly')
 
-print recipes
-print type(recipes)
+# cisplay recipes
+print 'Recipes Loaded into System'
+meals = recipes.keys()
+for i in meals:
+    print '  ' + i
 
-pantry = ('salt', 'beer', 'bread', 'water', 'stock', 'pepper')
+# create a tupil of ingredients import from a csv file
+pantry = []
+for items in openascsv.opencsv('/users/lauren/downloads/ings.csv'):
+    pantry.extend([items])    
 
-print pantry
-print type(pantry)
-
-count = 0
-for ingredients in recipes['soup']:
-    a = ingredients in pantry
-    print a
-    if ingredients in pantry:
-        count = count+1
-print count
-recipelen = len(recipes['soup'])
-if count == recipelen:
-    print 'You have what you need for soup'
+# display ingredients
+print 'Ingredients Loaded into System'
+for i in pantry:
+    print '  ' + i
+    
+# nexted for loop to determine which recipes the ingredients in the pantry are
+# sufficient to create
+for keys in meals:  # keys are the names of the meals
+    count = 0                       # initiate zero count
+    for ingredients in recipes[keys]:   # for loop on ingreadients in the recipes       
+        if ingredients in pantry:   # check if ingreadients are in pantry
+            count = count+1         # if in pantry, add one to count
+    recipelen = len(recipes[keys])  # compare length of count to length of recipe 
+    if count == recipelen:          # if equal, you have all the ingredients
+        print 'You have what you need for ' + keys  # returns meals you have ingredients for
---
+++ openascsv.py
@@ -0,0 +1,14 @@
+import csv
+import sys
+import re
+
+
+a = []
+
+def opencsv(filename):
+    with open(filename, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            a.extend(row)
+            #print ', '.join(row)
+    return a
